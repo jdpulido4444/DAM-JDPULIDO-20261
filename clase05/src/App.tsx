f28ceceadc12c872c8f4095f622ab268712963e7
jdpulido4444 <jdpulido@unbosque.edu.co>
2026-03-11 19:45:46 -0500

    cambio calculadora

--- clase05/src/App.tsx
+++ clase05/src/App.tsx
@@ -7,41 +7,71 @@ import { CountScreen } from './Components';
 import React from 'react';
 import { colors } from './themes';
 import { appStyles } from './App.style';
+import { Text } from 'react-native';
 
 
+type AppProps = {}
 
-type AppProps = {
-  atributo: String;
-}
-let count: number = 0;
-
-
-const handleOnPress = (value: number) => {
-  console.log("Press Button")
-  count += value;
-  console.log(count)
+type AppState = {
+  result: number;
 }
 
+export class App extends React.Component<AppProps, AppState> {
 
-export class App extends React.Component <AppProps> {
+  constructor(props: AppProps){
+    super(props)
+
+    this.state = {
+      result: 0
+    }
+  }
+
+  handleOperation = (value: number) => {
+    this.setState({
+      result: this.state.result + value
+    })
+  }
+
+  multiply = () => {
+    this.setState({
+      result: this.state.result * 2
+    })
+  }
+
+  divide = () => {
+    this.setState({
+      result: this.state.result / 2
+    })
+  }
+
+  reset = () => {
+    this.setState({
+      result: 0
+    })
+  }
+
   render(){
 
-    const label = "hello world"
-    const name = "hello world x2"
+    return(
 
-    console.log("App Funcionando")
-   return (
+      <View style={appStyles.safeArea}>
+        <View style={appStyles.container}>
 
-          <View style={appStyles.safeArea}>
-            <view style={appStyles.container}>
-            <CountScreen label={label} OnPress={()=>handleOnPress(1)}></CountScreen>
-            <CountScreen label={name} OnPress={()=>handleOnPress(2)}></CountScreen>
-            </view>
-          </View>
+          <Text>Resultado: {this.state.result}</Text>
 
-     
-      );
+          <CountScreen label="+1" OnPress={() => this.handleOperation(1)} />
+          <CountScreen label="-1" OnPress={() => this.handleOperation(-1)} />
+
+          <CountScreen label="x2" OnPress={this.multiply} />
+          <CountScreen label="/2" OnPress={this.divide} />
+
+          <CountScreen label="Reset" OnPress={this.reset} />
+
+        </View>
+      </View>
+
+    )
   }
 }
 
-export default App;
+export default App
